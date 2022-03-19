1852868a168e2e02475055d81fa68379e1d7b7bd
Christophe R Patraldo <patrouch@gmail.com>
2022-03-19 10:51:41 -0400

    changed font and bg color, again

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-    <link href="https://fonts.googleapis.com/css?family=Gelasio" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Changa">
 
 </svelte:head>
@@ -24,15 +24,15 @@
 <svelte:body />
 <style>
     @font-face {
-        font-family: 'Gelasio';
+        font-family: 'Montserrat';
         font-style: normal;
         font-weight: 400;
-        src: local('Gelasio Regular'), local('Gelasio-Regular'), url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
+        src: local('Montserrat Regular'), local('Montserrat-Regular'), url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
         unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
 
     h1 {
-        font-family: Gelasio
+        font-family: Montserrat
 
     }
     button {
@@ -42,7 +42,7 @@
     font-family: "Changa", sans-serif;
     }
     :global body {
-  background-color: orangered;
+  background-color: lightblue;
   padding: 2%;
 }
 
